3caaefeb6fddd793c44b61d4b16bc4fd3548aef4
Peter <ptbdnr@gmail.com>
2025-04-08 11:09:55 +0100

    chore: improve content

--- front/src/components/instruction.py
+++ front/src/components/instruction.py
@@ -1,14 +1,30 @@
 import streamlit as st
+from textwrap import dedent
 
 
 def component() -> None:
     """Main component for the tab."""
-    st.header("Simple RAG Chatbot")
+    st.header("Using the App")
 
-    st.markdown(
+    st.markdown(dedent(
         """
-        ### Instructions
-        - **Step 1**: Enter your prompt in the chat input box.
-        - **Step 2**: Click on the "Send" button to generate a response.
+        This tab provides a brief overview of how operate the app:
+
+        ## Knowledge Tab
+        Use this tab to interact with the knowledge store:
+
+        * Refresh Knowledge Store: Click the refresh button to query the backend store for available documents linked to your tenant ID.
+        * Load Document: Provide a document URL or text input to load a new document into the system. The app supports various MIME types.
+        * Split Document: Provide a document ID to split the document into manageable chunks.
+        * Encode Document Chunks: After splitting, encode the document chunks for further processing.
+        
+        ## Chat Tab
+        Engage with the chatbot:
+
+        Your conversation (both your input and the assistant's responses) is maintained throughout your session.
+        
+        Use either batch processing or streaming responses to interact with the OpenAI-powered backend.
+        
+        Enjoy exploring the different features of the application!
         """
-    )
+    ))
